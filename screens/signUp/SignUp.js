9e347e78319d4cfdad848de88b6cecc66d0f0527
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 15:31:09 +0100

    [dev]: Add: Firebase Auth

--- screens/signUp/SignUp.js
+++ screens/signUp/SignUp.js
@@ -1,12 +1,105 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {CustomInput, TextComponent} from '../../components';
+import {COLORS, FONTS, SIZES} from '../../global';
+import {useDispatch, useSelector} from 'react-redux';
+import {addAuth} from '../../redux/AuthenticationSlice';
+import {nanoid} from '@reduxjs/toolkit';
+import {selectAllAuth} from '../../redux/AuthenticationSlice';
+import {auth} from '../../firebase';
 
-const SignUp = () => {
+const SignUp = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const onSaveAuth = () => {
+    let success = false;
+    if (email && password && confirmPassword && password === confirmPassword) {
+      if (emailRegex.test(email)) {
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+
+        auth
+          .createUserWithEmailAndPassword(email, password)
+          .then(userCredential => {
+            const user = userCredential.user;
+          })
+          .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+          });
+
+        success = true;
+      } else {
+        alert('Bad Email:Provide a valid Email');
+        success = false;
+      }
+    } else {
+      alert('Bad Email or Password mismatch');
+      success = false;
+    }
+    console.log(success);
+    return success;
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Sign Up</Text>
+      <CustomInput placeholder="Email" value={email} setValue={setEmail} />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+      />
+
+      <CustomInput
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        setValue={setConfirmPassword}
+      />
+
+      <TextComponent
+        contentContainerStyle={styles.containerStyleOne}
+        label="Sign Up"
+        onPress={() => {
+          onSaveAuth();
+          // && navigation.replace('Login');
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: SIZES.height,
+  },
+
+  welcome: {
+    fontSize: SIZES.h1,
+    marginVertical: SIZES.margin,
+    ...FONTS.h1,
+    color: COLORS.darkGreen,
+  },
+  containerStyle: {
+    backgroundColor: null,
+  },
+  containerStyleOne: {
+    height: 50,
+    borderRadius: SIZES.radius,
+    paddingHorizontal: 80,
+    marginVertical: SIZES.margin,
+    backgroundColor: COLORS.darkGreen,
+  },
+});
 
 export default SignUp;
