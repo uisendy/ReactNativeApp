d84406f62636f656933f0b3274affe1338de027c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:26:35 +0100

    [dev]: modify: Global files - Data

---
+++ global/data.js
@@ -0,0 +1,22 @@
+const walkthrough = [
+  {
+    id: 0,
+    title: 'Collect of Great meals',
+    image: require('../assets/images/walkthrough/walkthrough1.jpg'),
+    sub_title: 'Share with friends and Love ones',
+  },
+  {
+    id: 1,
+    title: 'Interactive',
+    image: require('../assets/images/walkthrough/walkthrough2.jpg'),
+    sub_title: 'See what others are making, like and leave comments',
+  },
+  {
+    id: 2,
+    title: 'Create That Recipe',
+    image: require('../assets/images/walkthrough/walkthrough3.jpg'),
+    sub_title: 'Others can follow, try, like and review what you make',
+  },
+];
+
+export default {walkthrough};
