7db77290d4fff6e70ae7802ce34a003c6c1ce3ba
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:26:15 +0100

    [dev]: modify: Global files - Icons

---
+++ global/icons.js
@@ -0,0 +1,17 @@
+const bookmark = require('../assets/icons/bookmark.png');
+const bookmarkFilled = require('../assets/icons/bookmark-filled.png');
+const home = require('../assets/icons/home.png');
+const search = require('../assets/icons/search.png');
+const settings = require('../assets/icons/settings.png');
+const back = require('../assets/icons/back.png');
+const rightArrow = require('../assets/icons/right-arrow.png');
+
+export default {
+  bookmark,
+  bookmarkFilled,
+  back,
+  home,
+  search,
+  settings,
+  rightArrow,
+};
