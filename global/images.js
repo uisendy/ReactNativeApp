67508ba542dcd1c23ccc5c5fadf31a2e7253b106
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:26:06 +0100

    [dev]: modify: Global files - Images

---
+++ global/images.js
@@ -0,0 +1,18 @@
+const loginBackground = require('../assets/images/login-background.png');
+const loginBackground2 = require('../assets/images/login-background-2.jpg');
+
+const recipe = require('../assets/images/recipes/recipe.png');
+const spagetti = require('../assets/images/recipes/spagetti.png');
+const satay = require('../assets/images/recipes/satay.png');
+const laksa = require('../assets/images/recipes/laksa.png');
+const nasiLemak = require('../assets/images/recipes/nasi-lemak.webp');
+
+export default {
+  loginBackground,
+  loginBackground2,
+  recipe,
+  spagetti,
+  satay,
+  laksa,
+  nasiLemak,
+};
