1b580f174f5f176c978af52fe6cf1aeeb4f45e98
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:26:45 +0100

    [dev]: modify: Global files - ApiData

---
+++ global/apiData.js
@@ -0,0 +1,9 @@
+const urlMap = {
+  random: 'https://www.themealdb.com/api/json/v1/1/random.php',
+  categories: 'https://www.themealdb.com/api/json/v1/1/categories.php',
+  byCategory: 'https://www.themealdb.com/api/json/v1/1/filter.php?c=',
+  byMealId: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=',
+  byName: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+};
+
+export default urlMap;
