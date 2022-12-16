e6f8e8b31c7d24f79c264f0e4c8b8cc0aa1aabe6
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:25:56 +0100

    [dev]: modify: Global files - Local Dummy Data

---
+++ global/recentRecipeData.js
@@ -0,0 +1,46 @@
+const recentRecipes = [
+  {
+    idMeal: '52978',
+    strMeal: 'Kumpir',
+    strThumbNail:
+      'https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg',
+    tags: 'SideDish | creamy',
+    serving: 1,
+    isBookmark: false,
+    category: 'Side',
+  },
+  {
+    idMeal: '53021',
+    strMeal: 'Malaysian Chicken Satay',
+    strThumbNail:
+      'https://www.themealdb.com/images/media/meals/q8sp3j1593349686.jpg',
+    tags: 'simple | beefy',
+    serving: 10,
+    isBookmark: true,
+    category: 'Beef',
+  },
+  {
+    idMeal: '52981',
+    strMeal: 'Snert (Dutch Split Pea Soup)',
+    strThumbNail:
+      'https://www.themealdb.com/images/media/meals/9ptx0a1565090843.jpg',
+    tags: 'Soup | Cake',
+    serving: 1,
+    isBookmark: true,
+    category: 'Side',
+  },
+  {
+    idMeal: '52764',
+    strMeal: 'Garides Saganaki',
+    strThumbNail:
+      'https://www.themealdb.com/images/media/meals/wuvryu1468232995.jpg',
+    tags: 'Seafood | Shellfish',
+    serving: 10,
+    isBookmark: false,
+    category: 'Seafood',
+  },
+];
+
+export default {
+  recentRecipes,
+};
