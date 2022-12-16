24b43c2a11a127314b249c1ed5d18378b8993742
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 23:34:16 +0100

    [dev]: Modify: Add Bookmark, settings and search screens

--- screens/index.js
+++ screens/index.js
@@ -5,5 +5,19 @@ import Login from './login/Login';
 import Home from './Home';
 import Recipe from './Recipe';
 import RecipeCategory from './RecipeCategory';
+import Bookmark from './Demo/Bookmark';
+import Settings from './Demo/Settings';
+import Search from './Demo/Search';
 
-export {Landing, Walkthrough, Login, SignUp, Home, Recipe, RecipeCategory};
+export {
+  Landing,
+  Walkthrough,
+  Login,
+  SignUp,
+  Home,
+  Recipe,
+  RecipeCategory,
+  Bookmark,
+  Settings,
+  Search,
+};
