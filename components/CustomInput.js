f19b84d85597b0982266496ec5228f53fdf0f735
Inieke Sendy <ini.e.sendy@gmail.com>
2022-12-16 05:28:29 +0100

    [dev]: modify: Components : - CustomInput

---
+++ components/CustomInput.js
@@ -0,0 +1,35 @@
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import React from 'react';
+import {COLORS} from '../global';
+
+const CustomInput = ({
+  value,
+  setValue,
+  placeholder,
+  secureTextEntry,
+  customStyle,
+}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+        style={{...customStyle}}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: COLORS.darkLime,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 5,
+    minWidth: '87%',
+  },
+});
+
+export default CustomInput;
